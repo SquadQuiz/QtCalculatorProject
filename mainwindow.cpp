8c93dcc075ccee8136b9e221df2fea78bad91187
SquadQuiz <amornsakstp6@gmail.com>
2023-04-23 19:15:20 +0700

    Added connection/function for Unary operation +/- and %

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -9,6 +9,7 @@ MainWindow::MainWindow(QWidget *parent)
 {
     ui->setupUi(this);
 
+    // number buttons
     connect(ui->pushButton_0, SIGNAL(released()), this, SLOT(digit_pressed()));
     connect(ui->pushButton_1, SIGNAL(released()), this, SLOT(digit_pressed()));
     connect(ui->pushButton_2, SIGNAL(released()), this, SLOT(digit_pressed()));
@@ -19,6 +20,10 @@ MainWindow::MainWindow(QWidget *parent)
     connect(ui->pushButton_7, SIGNAL(released()), this, SLOT(digit_pressed()));
     connect(ui->pushButton_8, SIGNAL(released()), this, SLOT(digit_pressed()));
     connect(ui->pushButton_9, SIGNAL(released()), this, SLOT(digit_pressed()));
+
+    // unary operation buttons.
+    connect(ui->pushButton_plusMinus, SIGNAL(released()), this, SLOT(unary_operation_pressed()));
+    connect(ui->pushButton_percent, SIGNAL(released()), this, SLOT(unary_operation_pressed()));
 }
 
 MainWindow::~MainWindow()
@@ -30,17 +35,53 @@ void MainWindow::digit_pressed()
 {
     QPushButton * button = (QPushButton*)sender();
 
-    // check which button signal was received.
-    qDebug() << button->text();
-
     double labelNumber;
     QString newLabel;
 
-//    labelNumber = (button->text()).toDouble(); // without new string append.
     labelNumber = (ui->label->text() + button->text()).toDouble();
-
     newLabel = QString::number(labelNumber, 'g', 15);
 
-//    ui->label->setText(button->text()); // set label text "Single character"
     ui->label->setText(newLabel);
+}
+
+void MainWindow::on_pushButton_decimal_released()
+{
+    ui->label->setText(ui->label->text() + ".");
+}
+
+void MainWindow::unary_operation_pressed()
+{
+    QPushButton *button = (QPushButton*)sender();
+    QString newLabel;
+    double labelNumber;
+
+    if (button->text() == "+/-")
+    {
+        labelNumber = ui->label->text().toDouble();
+        labelNumber = labelNumber * -1;
+        newLabel = QString::number(labelNumber, 'g', 15);
+        ui->label->setText(newLabel);
+    }
+    if (button->text() == "%")
+    {
+        labelNumber = ui->label->text().toDouble();
+        labelNumber = labelNumber * 0.01;
+        newLabel = QString::number(labelNumber, 'g', 15);
+        ui->label->setText(newLabel);
+    }
+}
+
+void MainWindow::on_pushButton_clear_released()
+{
+
+}
+
+void MainWindow::on_pushButton_equals_released()
+{
+
+}
+
+void MainWindow::binary_operation_pressed()
+{
+
 }
--- mainwindow.h
+++ mainwindow.h
@@ -20,5 +20,10 @@ private:
 
 private slots:
     void digit_pressed();
+    void on_pushButton_decimal_released();
+    void unary_operation_pressed();
+    void on_pushButton_clear_released();
+    void on_pushButton_equals_released();
+    void binary_operation_pressed();
 };
 #endif // MAINWINDOW_H
